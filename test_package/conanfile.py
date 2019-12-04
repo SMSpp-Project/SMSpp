bdd8587e6fe295e1d0398e1ea9aee2cd4a496bff
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2019-12-04 11:22:19 +0100

    Update Conan recipe

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,16 +1,13 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, CMake, tools
 
-
-class SmsppTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
 
@@ -21,5 +18,5 @@ class SmsppTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            os.chdir("bin")
-            self.run(".%sexample" % os.sep)
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
