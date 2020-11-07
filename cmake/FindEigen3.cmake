92b8dde9825e2fa66ddbfb3aa20ef0d41745b574
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2020-11-07 23:00:07 +0100

    Revise CMake files

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -1,12 +1,11 @@
 # --------------------------------------------------------------------------- #
-#    CMake find module for Eigen3                                             #
+#    Custom CMake find module for Eigen3                                      #
 #                                                                             #
 #    This find module is provided because the user may not have Eigen3        #
-#    find module installed in the system default directories.                 #
-#    It is a wrapper that tries to find Eigen3 manually in case the           #
-#    find module provided by Eigen3 is not found or fails.                    #
+#    configuration file installed in the system default directories, which    #
+#    happens to be the case for some of our main developers and testers.      #
 #                                                                             #
-#    Accepts the following HINT:                                              #
+#    Accepts the following PATH:                                              #
 #                                                                             #
 #    - EIGEN3_ROOT - Custom Eigen3 root directory                             #
 #                                                                             #
@@ -21,21 +20,36 @@
 #                         Dipartimento di Informatica                         #
 #                             Universita' di Pisa                             #
 # --------------------------------------------------------------------------- #
-# TODO: Support version check
-# Try to find a CMake-built Eigen3 ------------------------------------------ #
-find_package(Eigen3 CONFIG QUIET)
-if (Eigen3_FOUND)
-    return()
+include(FindPackageHandleStandardArgs)
+
+# ----- Find the headers ---------------------------------------------------- #
+find_path(EIGEN3_INCLUDE_DIR Eigen/Dense
+          PATHS ${EIGEN3_ROOT}
+          DOC "Eigen3 include directory")
+
+# ----- Parse the version --------------------------------------------------- #
+if (EIGEN3_INCLUDE_DIR)
+    file(STRINGS
+         "${EIGEN3_INCLUDE_DIR}/Eigen/src/Core/util/Macros.h"
+         _eigen3_version_lines REGEX "#define EIGEN_(WORLD|MAJOR|MINOR)_VERSION")
+
+    string(REGEX REPLACE ".*EIGEN_WORLD_VERSION *\([0-9]*\).*" "\\1" _eigen3_version_major "${_eigen3_version_lines}")
+    string(REGEX REPLACE ".*EIGEN_MAJOR_VERSION *\([0-9]*\).*" "\\1" _eigen3_version_minor "${_eigen3_version_lines}")
+    string(REGEX REPLACE ".*EIGEN_MINOR_VERSION *\([0-9]*\).*" "\\1" _eigen3_version_patch "${_eigen3_version_lines}")
+
+    set(EIGEN3_VERSION "${_eigen3_version_major}.${_eigen3_version_minor}.${_eigen3_version_patch}")
+    unset(_eigen3_version_lines)
+    unset(_eigen3_version_major)
+    unset(_eigen3_version_minor)
+    unset(_eigen3_version_patch)
 endif ()
 
-# Try to find a Eigen3 "manually" ------------------------------------------- #
-find_path(EIGEN3_INCLUDE_DIR Eigen/Dense
-          PATHS ${EIGEN3_ROOT})
-mark_as_advanced(EIGEN3_INCLUDE_DIR)
+# ----- Handle the standard arguments --------------------------------------- #
+find_package_handle_standard_args(Eigen3
+                                  REQUIRED_VARS EIGEN3_INCLUDE_DIR
+                                  VERSION_VAR EIGEN3_VERSION)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Eigen3 REQUIRED_VARS EIGEN3_INCLUDE_DIR)
-
+# ----- Export the target --------------------------------------------------- #
 if (Eigen3_FOUND)
     set(EIGEN3_INCLUDE_DIRS "${EIGEN3_INCLUDE_DIR}")
 
@@ -46,3 +60,5 @@ if (Eigen3_FOUND)
                 INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIR}")
     endif ()
 endif ()
+
+mark_as_advanced(EIGEN3_INCLUDE_DIR EIGEN3_VERSION)
--- cmake/FindnetCDFCxx.cmake
+++ cmake/FindnetCDFCxx.cmake
@@ -1,13 +1,20 @@
 # --------------------------------------------------------------------------- #
-#    CMake find module for netCDF-C++                                         #
+#    Custom CMake find module for netCDF-C++                                  #
 #                                                                             #
-#    Accepts the following HINTS:                                             #
+#    This find module is provided because the CMake support from netCDF-C++   #
+#    was found to be lacking. In particular, it appears that netCDFCxx does   #
+#    not come with a CMake configuration (netCDFCxxConfig.cmake).             #
+#                                                                             #
+#    Accepts the following PATHS:                                             #
 #                                                                             #
 #    - NETCDFCXX_INC - Custom path to netCDF-C++ headers                      #
 #    - NETCDFCXX_LIB - Custom path to netCDF-C++ libraries                    #
 #                                                                             #
-#    Provides the following imported targets:                                 #
+#    Provides (at least) the following variables:                             #
 #                                                                             #
+#    - netCDFCxx_FOUND - Whether netCDF-C++ was found or not                  #
+#    - netCDFCxx_INCLUDE_DIRS - Include directories                           #
+#    - netCDFCxx_LIBRARIES - Libraries to link                                #
 #    - netCDF::netCDFCxx - A target to use with target_link_libraries()       #
 #                                                                             #
 #                              Niccolo' Iardella                              #
@@ -20,28 +27,26 @@ include(FindPackageHandleStandardArgs)
 # ----- Requirements -------------------------------------------------------- #
 find_package(netCDF REQUIRED)
 
-# ----- netCDF-C++ ---------------------------------------------------------- #
-# Find headers
-find_path(netCDFCxx_INCLUDE_DIR
-          NAMES netcdf
-          HINTS ${NETCDFCXX_INC}
-          DOC "netCDF-C++ include directories")
-mark_as_advanced(netCDFCxx_INCLUDE_DIR)
+# ----- Find the headers and library ---------------------------------------- #
+find_path(netCDFCxx_INCLUDE_DIR netcdf
+          PATHS ${NETCDFCXX_INC}
+          DOC "netCDF-C++ include directory")
 
-# Find library
 find_library(netCDFCxx_LIBRARY
              NAMES netcdf-cxx4 netcdf_c++4
-             HINTS ${NETCDFCXX_LIB}
+             PATHS ${NETCDFCXX_LIB}
              DOC "netCDF-C++ library")
-mark_as_advanced(netCDFCxx_LIBRARY)
 
-# Handle standard arguments
+# Get version from netCDF (there is no way to parse it from the headers)
+set(netCDFCxx_VERSION ${netCDF_VERSION})
+
+# ----- Handle the standard arguments --------------------------------------- #
 find_package_handle_standard_args(
         netCDFCxx
         REQUIRED_VARS netCDFCxx_LIBRARY netCDFCxx_INCLUDE_DIR
-        VERSION_VAR netCDF_VERSION)
+        VERSION_VAR netCDFCxx_VERSION)
 
-# Define target
+# ----- Export the target --------------------------------------------------- #
 if (netCDFCxx_FOUND)
     set(netCDFCxx_INCLUDE_DIRS "${netCDFCxx_INCLUDE_DIR}")
     set(netCDFCxx_LIBRARIES "${netCDFCxx_LIBRARY}")
@@ -55,3 +60,7 @@ if (netCDFCxx_FOUND)
                 INTERFACE_LINK_LIBRARIES "netCDF::netcdf")
     endif ()
 endif ()
+
+mark_as_advanced(netCDFCxx_INCLUDE_DIR
+                 netCDFCxx_LIBRARY
+                 netCDFCxx_VERSION)
