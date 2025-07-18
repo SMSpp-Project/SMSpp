cafd6a5ae58d498437fc35bb1245442fff5c67cc
dmeoli <donato.meoli.95@gmail.com>
2025-07-18 18:20:04 +0200

    fixed ClangPatch.cmake

--- cmake/ClangPatch.cmake
+++ cmake/ClangPatch.cmake
@@ -1,8 +1,12 @@
 # This definition is needed because the version of Clang shipped with
 # macOS Big Sur has an issue with boost::any type recognition.
+# 12.0.0 ≤ AppleClang < 12.1.0
+# CMAKE_SYSTEM_VERSION ≤ 20.2.0
 
-if (${CMAKE_CXX_COMPILER_ID} MATCHES "AppleClang" AND
-    ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND
-    ${CMAKE_SYSTEM_VERSION} VERSION_GREATER_EQUAL 20.1.0)
+if (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang" AND
+    CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "12.0.0" AND
+    CMAKE_CXX_COMPILER_VERSION VERSION_LESS "12.1.0" AND
+    CMAKE_SYSTEM_NAME STREQUAL "Darwin" AND
+    CMAKE_SYSTEM_VERSION VERSION_LESS_EQUAL "20.2.0")
     target_compile_definitions(${modName} PUBLIC CLANG_1200_0_32_27_PATCH)
 endif ()
