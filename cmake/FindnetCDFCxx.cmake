0a8a3a44b2b153679a3fb98fd87e2442ac0d57d9
dmeoli <donato.meoli.95@gmail.com>
2025-07-27 04:46:28 +0200

    rollback cmake file

--- cmake/FindnetCDFCxx.cmake
+++ cmake/FindnetCDFCxx.cmake
@@ -75,7 +75,7 @@ else ()
         find_library(netCDFCxx_LIBRARY
                      NAMES netcdf-cxx4
                      PATHS ${netCDFCxx_ROOT}/lib
-                           $ENV{LIBRARY_PREFIX}/lib
+                           $ENV{LIBRARY_LIB}
                      NO_DEFAULT_PATH
                      DOC "netCDF-C++ library.")
 
